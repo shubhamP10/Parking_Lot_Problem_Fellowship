d56e6abc07533310f93fa6149c18fd58d247a75d
“Shubham” <shubhampattar2@gmail.com>
2020-07-17 20:54:01 +0530

    ADDED Test Case To Check For Vehicle is Already Parked or Not.

--- src/test/java/com/bridgelabz/parkingmanagement/ParkingLotManagementTest.java
+++ src/test/java/com/bridgelabz/parkingmanagement/ParkingLotManagementTest.java
@@ -1,14 +1,30 @@
 package com.bridgelabz.parkingmanagement;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotManagementTest {
 
+    ParkingLot parkingLot = null;
+    Object vehicle = null;
+
+    @Before
+    public void setUp() throws Exception {
+        parkingLot = new ParkingLot();
+        vehicle = new Object();
+    }
+
     @Test
     public void givenVehicle_WhenPark_ShouldReturnTrue() {
-        ParkingLot parkingLot = new ParkingLot();
-        boolean parkingStatus = parkingLot.parkVehicle(new Object());
+        boolean parkingStatus = parkingLot.parkVehicle(vehicle);
         Assert.assertTrue(parkingStatus);
+    }
+
+    @Test
+    public void givenVehicle_WhenAlreadyParked_ShouldReturnFalse() {
+        parkingLot.parkVehicle(vehicle);
+        boolean parkingStatus = parkingLot.parkVehicle(vehicle);
+        Assert.assertFalse(parkingStatus);
     }
 }
